a289480acbfbdcdf5277c97192732d0a2a5368ca
LaukikOnMac <22937426+LRagji@users.noreply.github.com>
2019-09-21 17:26:09 +0530

    Fixed linting and new changes

--- transpiler/entry-point.js
+++ transpiler/entry-point.js
@@ -1,41 +1,52 @@
 // Copyright © 2019 Baker Hughes, a GE company, LLC.  All rights reserved
 const configOptions = require('./magic-tool.json');
-const fileSystem = require('fs');
+const _logger = require('./lou-util');
 const spawn = require('child_process').spawn;
 
 async function main() {
+    const backgroundProcess = [];
     for (let cmdPointer = 0; cmdPointer < configOptions.project.commands.length; cmdPointer++) {
         const cmdlet = configOptions.project.commands[cmdPointer].cmd;
         const options = configOptions.project.commands[cmdPointer].options || {};
         const args = configOptions.project.commands[cmdPointer].args;
-        console.log('\n\r \n\r' + '[' + cmdPointer + ']' + 'Executing Command:' + cmdlet + '\n\r');
+        const isBackgroundProcess = configOptions.project.commands[cmdPointer].keepRunningInBackgroud;
+        _logger.log('\n\r \n\r' + '[' + cmdPointer + ']' + 'Executing Command:' + cmdlet + '\n\r');
         try {
-            await runCommand(cmdlet, args, options);
+            if (isBackgroundProcess === false) {
+                await runCommand(cmdlet, args, options);
+            } else {
+                backgroundProcess.push(runCommand(cmdlet, args, options));
+            }
         } catch (err) {
-            console.log('Command Errored:' + JSON.stringify(err));
+            _logger.log('Command Errored:' + JSON.stringify(err));
         }
     }
+
+    return backgroundProcess;
 }
 
 async function runCommand(command, args, options) {
     return new Promise((acc, rej) => {
         const cmd = spawn(command, args, options);
         cmd.stdout.on('data', data => {
-            console.log(`${data}`);
+            _logger.log(`${data}`);
         });
 
         cmd.stderr.on('data', data => {
-            console.log(`${data}`);
+            _logger.log(`${data}`);
         });
 
         cmd.on('close', code => {
             if (code === 0) {
                 acc(code);
             } else {
-                acc(code);
+                rej(code);
             }
         });
     });
 }
 
-main().then(() => console.log('Execution Done')).catch(err => console.log('Process level Catch:' + JSON.stringify(err)));
+main().then(longRunningProcesses => {
+    _logger.log('Execution Done, waiting for background processes');
+    Promise.All(longRunningProcesses);
+}).catch(err => _logger.log('Process level Catch:' + JSON.stringify(err)));
---
+++ transpiler/log-util.js
@@ -0,0 +1,7 @@
+// Copyright © 2019 Baker Hughes, a GE company, LLC.  All rights reserved
+module.export = class logger {
+    static log(message) {
+        // tslint:disable-next-line: no-console
+        console.log(message);
+    }
+};
