1d1462701e5a1a19bb1aa50c85387c97f3b9c0b9
LaukikOnWinLab <22937426+LRagji@users.noreply.github.com>
2019-11-16 20:23:50 +0530

    Added accordion component

---
+++ transpiler/elements/bs_accordion.js
@@ -0,0 +1,14 @@
+class bs_accordion {
+    constructor() {
+        this.package = {
+            execute: ['npx ng add ngx-bootstrap --component accordion'],
+            moduleImports: [{ moduleName: 'AccordionModule', link: 'ngx-bootstrap/accordion' }, { moduleName: 'CommonModule', link: '@angular/common' }]
+        };
+        this.defaultProperties = { text: 'No Name' };
+        this.template = this.template.bind(this);
+    }
+
+    async template(props, layoutBuilder) {
+        return `<accordion><accordion-group heading="${props.name}">This content is straight in the template.</accordion-group></accordion>`;
+    }
+}
