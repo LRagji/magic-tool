523cc8e40ad20b96b1b88e9fc6433e2654d28deb
LaukikOnWinLab <22937426+LRagji@users.noreply.github.com>
2019-11-16 20:19:27 +0530

    Added bs_button component

---
+++ transpiler/elements/bs_button.js
@@ -0,0 +1,14 @@
+class bs_button {
+    constructor() {
+        this.package = {
+            execute: ['npx ng add ngx-bootstrap --component buttons'],
+            moduleImports: [{ moduleName: 'ButtonsModule', link: 'ngx-bootstrap/buttons' }]
+        };
+        this.defaultProperties = { text: 'No Name' };
+        this.template = this.template.bind(this);
+    }
+
+    async template(props, layoutBuilder) {
+        return `<button type="button" class="btn btn-primary">${props.text}</button>`;
+    }
+}
