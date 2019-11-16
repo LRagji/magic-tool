50d958bddf37d4e95230dfe8185478a7c46caead
LaukikOnWinLab <22937426+LRagji@users.noreply.github.com>
2019-11-16 19:48:06 +0530

    Rectified the repo and refactored

--- element-repository/element-repository.js
+++ element-repository/element-repository.js
@@ -3,21 +3,21 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
+const defaultPort = 3000;
+const defaultPath = "/Users/laukikragji/Documents/Git/Local/magic-tool/transpiler/elements/";
 
 class ElementRepository {
     constructor() {
         this.entryPoint = this.entryPoint.bind(this);
         this.fetchelement = this.fetchelement.bind(this);
-        this.repoPath = "/Users/laukikragji/Documents/Git/Local/magic-tool/transpiler/elements/";
-        this.port = 3000;
     }
 
     entryPoint(args) {
-        this.repoPath = this.repoPath || args[1];
-        this.port = this.port || args[2];
+        this.repoPath = args[2] || process.env.REPOPATH || defaultPath;
+        this.port = args[3] || process.env.REPOPORT || defaultPort;
         app.use(bodyParser.urlencoded({ extended: false }))
         app.get('/v1/elements/:elementId', this.fetchelement)
-        app.listen(this.port, () => console.log(`Elements repo active on ${this.port}!`))
+        app.listen(this.port, () => console.log(`Elements repo active on ${this.port}! with repo from:${this.repoPath}`))
     }
 
     fetchelement(req, res) {
@@ -32,11 +32,10 @@ class ElementRepository {
                     }
                 }
                 res.sendFile(elementPath, options);
-                console.log("Fetched Element:" + elementId)
             }
             else {
-                res.status(404).send('Cannot find element with id' + req.params.elementId.toLowerCase());
-                console.warn("Missing Element:" + elementId)
+                res.status(404).send('Cannot find element with id: ' + req.params.elementId.toLowerCase());
+                console.warn("Missing Element:" + elementId + "@" + elementPath)
             }
         }
         catch (ex) {
