5bd7ad7da0b61e15c0b14efee62d2bcb7ec0f74b
LaukikOnWinLab <22937426+LRagji@users.noreply.github.com>
2019-11-19 00:39:04 +0530

    Completed Topic Based Que Executor

---
+++ service/topic-que.js
@@ -0,0 +1,81 @@
+let Queue = require('better-queue');
+let cache = require('memory-cache');
+let shortid = require('shortid');
+
+module.exports = class TopicQue {
+    constructor(cleanResultsAfter = 60000) {
+        this._clearResultTimeout = cleanResultsAfter;
+        this._topicQue = new Map();
+        this._activeTasks = new cache.Cache();
+
+        this.enque = this.enque.bind(this);
+        this.status = this.status.bind(this);
+        this._createQue = this._createQue.bind(this);
+        this._processingCompleted = this._processingCompleted.bind(this);
+        this._taskCompleted = this._taskCompleted.bind(this);
+        this._taskFailed = this._taskFailed.bind(this);
+        this._taskQued = this._taskQued.bind(this);
+        this._taskStarted = this._taskStarted.bind(this);
+    }
+
+    enque(topicName, task) {
+        if (!this._topicQue.has(topicName)) {
+            const q = this._createQue(topicName);
+            this._topicQue.set(topicName, q);
+        }
+
+        const taskWrapper = { id: topicName + shortid.generate(), payload: task };
+
+        this._topicQue.get(topicName).push(taskWrapper);
+        return taskWrapper.id;
+    }
+
+    status(taskId) {
+        let task = this._activeTasks.get(taskId);
+        return task == undefined ? undefined : task.status;
+    }
+
+    _createQue(topicName) {
+        var q = new Queue(function (input, cb) {
+            try {
+                let result = input.payload();
+                cb(null, result);
+            }
+            catch (err) {
+                cb(err, null);
+            }
+        }, { afterProcessDelay: 1000 });
+
+        q.on('drain', () => this._processingCompleted(topicName));
+        q.on('task_finish', this._taskCompleted);
+        q.on('task_failed', this._taskFailed);
+        q.on('task_queued', this._taskQued);
+        q.on('task_started', this._taskStarted);
+
+        return q
+    }
+
+    _processingCompleted(topicName) {
+        this._topicQue.delete(topicName);
+    }
+
+    _taskQued(taskId, X, Y) {
+        this._activeTasks.put(taskId, { status: "Enqued" });
+    }
+
+    _taskStarted(taskId, X, Y) {
+        //This seems to be timed event so it can occur even after taskcompleted event if the task is small enough.
+        let task = this._activeTasks.get(taskId);
+        if (task !== undefined && (task.status !== "Completed" && task.status !== "Error")) {
+            this._activeTasks.put(taskId, { status: "Executing" });
+        }
+    }
+
+    _taskCompleted(taskId, result, stats) {
+        this._activeTasks.put(taskId, { status: "Completed", result: result, stats: stats }, this._clearResultTimeout);
+    }
+
+    _taskFailed(taskId, err, stats) {
+        this._activeTasks.put(taskId, { status: "Error", err: err, stats: stats }, this._clearResultTimeout);
+    }
+}
