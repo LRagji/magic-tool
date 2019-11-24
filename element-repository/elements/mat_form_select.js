3f5a57cbcebbac0b9b41acd791c60d94136e3fbe
LaukikOnMac <22937426+LRagji@users.noreply.github.com>
2019-11-24 14:39:09 +0530

    Added a new mat select component dropdown

---
+++ element-repository/elements/mat_form_select.js
@@ -0,0 +1,24 @@
+class mat_form_select {
+    constructor() {
+        this.package = {
+            execute: ['npx ng add @angular/material --interactive=false --theme=custom --gestures=true --animations=true'],
+            moduleImports: [{ moduleName: 'MatSelectModule', link: '@angular/material/select' }]
+        },
+            this.defaultProperties = { label: 'Material Components', options: ["A", "B"] };
+        this.template = this.template.bind(this);
+    }
+
+    async template(props, layoutBuilder) {
+        const label = `<mat-label>${props.label}</mat-label>`;
+        let options = "";
+        props.options.forEach(element => {
+            options += `<mat-option value=${element}>${element}</mat-option>`
+        });
+        return `<mat-form-field>
+       ${props.label == undefined ? '' : label}
+        <mat-select>
+        ${options}
+        </mat-select>
+      </mat-form-field>`;
+    }
+}
