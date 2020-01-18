44b5a2e4ba0b105b7464929d6853b3145a6d0f02
LaukikOnMac <22937426+LRagji@users.noreply.github.com>
2020-01-18 17:28:34 +0530

    Added support for html span tag for heading element

--- element-repository/elements/html_span.js
+++ element-repository/elements/html_span.js
@@ -1,11 +1,25 @@
 class html_span {
     constructor() {
         this.package = {};
-        this.defaultProperties = { text: "Hello World" };
+        this.defaultProperties = { text: "Hello World", type="normal" };
         this.template = this.template.bind(this);
     }
 
     async template(props) {
-        return { "style": "", "html":`<span>${props.text}</span>`};
+        switch (props.type) {
+            case "heading1":
+                return { "style": "", "html": `<h1>${props.text}</h1>` };
+            case "heading2":
+                return { "style": "", "html": `<h2>${props.text}</h2>` };
+            case "heading3":
+                return { "style": "", "html": `<h3>${props.text}</h3>` };
+            case "heading4":
+                return { "style": "", "html": `<h4>${props.text}</h4>` };
+            case "heading5":
+                return { "style": "", "html": `<h5>${props.text}</h5>` };
+
+            default:
+                return { "style": "", "html": `<span>${props.text}</span>` };
+        }
     }
 }
