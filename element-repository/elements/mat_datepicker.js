34eb3a81ffea16612289e4ac6f13dfcb455a05fb
LaukikOnMac <22937426+LRagji@users.noreply.github.com>
2020-07-12 17:41:36 +0530

    Fixed date picker module

--- element-repository/elements/mat_datepicker.js
+++ element-repository/elements/mat_datepicker.js
@@ -2,20 +2,23 @@ class mat_datepicker {
     constructor() {
         this.package = {
             execute: ['npx ng add @angular/material --interactive=false --theme=custom --gestures=true --animations=true'],
-            moduleImports: [{ moduleName: 'MatDatepickerModule', link: '@angular/material/datepicker' }]
+            moduleImports: [
+                { moduleName: 'MatInputModule', link: '@angular/material' },
+                { moduleName: 'MatNativeDateModule', link: '@angular/material/core' },
+                { moduleName: 'MatFormFieldModule', link: '@angular/material/form-field' },
+                { moduleName: 'MatDatepickerModule', link: '@angular/material/datepicker' }]
         },
-            this.defaultProperties = { id: 'dp1', label:"Start Date" };
+            this.defaultProperties = { id: 'dp1', label: "Start Date" };
         this.template = this.template.bind(this);
     }
 
     async template(props, layoutBuilder) {
-        return { "style": "", "html": `<p>
-        <mat-form-field appearance="fill">
+        return {
+            "style": "", "html": `<mat-form-field appearance="fill">
           <mat-label>${props.label}</mat-label>
           <input matInput [matDatepicker]="${props.id}" >
           <mat-datepicker-toggle matSuffix [for]="${props.id}"></mat-datepicker-toggle>
           <mat-datepicker #${props.id} disabled="false"></mat-datepicker>
-        </mat-form-field>
-      </p>` };
+        </mat-form-field>` };
     }
 }
